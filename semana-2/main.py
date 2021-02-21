9a833f0fbab947e650fc94a7203083dfaf29ffd1
Sebastián Córdoba <sebascordobaa@hotmail.com>
2021-02-20 22:31:39 -0300

    JS validation repalced with WTForms

--- semana-2/main.py
+++ semana-2/main.py
@@ -2,6 +2,8 @@ from sassutils.wsgi import SassMiddleware
 import requests
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
+import forms
+
 
 app = Flask(__name__)
 app.wsgi_app = SassMiddleware(app.wsgi_app, {
@@ -14,26 +16,32 @@ def page_not_found(e):
     return render_template('404.html')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('base.html')
+    form = forms.SearchProfileForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        return redirect(url_for('profile', name=username))
+    return render_template('base.html', form=form)
 
 
 @app.route('/user/<name>')
 def profile(name):
-    request = requests.get(f'https://api.github.com/users/{name}')
-    profile = request.json()
+    form = forms.SearchProfileForm(request.form)
+    req = requests.get(f'https://api.github.com/users/{name}')
+    profile = req.json()
     if ('message' in profile):
         return redirect(url_for('error404'))
     else:
         date = datetime.strptime(
             profile['created_at'], "%Y-%m-%dT%H:%M:%SZ").date().strftime("%d/%m/%y")
-        return render_template('profile.html', profile=profile, date=date)
+        return render_template('profile.html', profile=profile, date=date, form=form)
 
 
 @app.route('/error')
 def error404():
-    return render_template('404.html')
+    form = forms.SearchProfileForm(request.form)
+    return render_template('404.html', form=form)
 
 
 if __name__ == '__main__':
