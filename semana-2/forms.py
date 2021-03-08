bd76376a46d55db6399cf45debf03c5f38f0cbb3
Sebastián Córdoba <sebascordobaa@hotmail.com>
2021-03-07 21:23:15 -0300

    Username dashes now are permited

--- semana-2/forms.py
+++ semana-2/forms.py
@@ -2,5 +2,5 @@ from wtforms import Form, StringField, validators
 
 
 class SearchProfileForm(Form):
-    username = StringField('Username', [validators.required(), validators.Length(min=1, max=25), validators.regexp(
-        r'^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$', message="Must only contain alphanumeric characters and dash")])
+    username = StringField('Username', [validators.required(), validators.Length(min=1, max=25), validators.Regexp(
+        r'^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$', flags=0, message="Must only contain alphanumeric characters and dash")])
