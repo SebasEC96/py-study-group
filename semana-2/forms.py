773c01756819e1dd22b18034cc6a19ed1f5e7b46
Sebastián Córdoba <sebascordobaa@hotmail.com>
2021-03-12 11:19:32 -0300

    Maximum length validator disabled

--- semana-2/forms.py
+++ semana-2/forms.py
@@ -2,5 +2,5 @@ from wtforms import Form, StringField, validators
 
 
 class SearchProfileForm(Form):
-    username = StringField('Username', [validators.required(), validators.Length(min=1, max=25), validators.Regexp(
+    username = StringField('Username', [validators.required(), validators.Length(min=1), validators.Regexp(
         r'^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$', flags=0, message="Must only contain alphanumeric characters and dash")])
